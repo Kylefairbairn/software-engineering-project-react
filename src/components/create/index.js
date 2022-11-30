f53f890fa0d0cf1a28ffd6f2654d3dbf0ed55492
kylefairbairn <kyle.fairbairn@gmail.com>
2022-11-30 16:59:46 -0500

    basic layout complete, need to update error handling and adding members/admins

--- src/components/create/index.js
+++ src/components/create/index.js
@@ -1,24 +1,12 @@
 import React, {useState} from "react";
-import {Form} from "react-bootstrap";
-import userEvent from "@testing-library/user-event";
-const CreateGroup = () => {
-    // need to create const to create form objects
-    // form validation
-        // creating multiple select for members and admin
-    // error handling
-    // probably need service from gino to create group
-        // from form object ?
-    // feedback that group has been created
-    // <label htmlFor="membersList">Members</label>
-    // <input type="text" className="form-control" id="membersList" placeholder="Members"/>
+import {Form, FormControl} from "react-bootstrap";
 
-    const currentForm = {
-        formMembers: '',
-        formDate: '',
-        admin: "",
-        groupName: "",
-        description: ''
-    }
+const CreateGroup = () => {
+
+    // error handling
+    // lists for admin and members
+    // connection to group service
+
 
     const [form, setForm] = useState({})
     const [errors, setErrors] = useState({})
@@ -32,72 +20,101 @@ const CreateGroup = () => {
     const handleAddMembers = (e) => {
         // probably going to have to find the user e
         // make a new user object
-
         const newMembers = {e}
-
         setMembers([...groupCreator,newMembers])
-
-        //setMembers((e) => ({...e, members}))
-
-        //console.log(members)
-
     }
 
-    console.log(groupCreator)
+    const handleCreateGroup = () => {
+        console.log(form)
+    }
 
-    //id="membersList" placeholder="ex. kyle fairbairn "/>
+    const setField = (field, value) => {
+        setForm({...form, [field]: value})
+
+        if(!!errors[field])
+            setErrors({...errors,[field]:null})
+    }
 
     return(
         <Form>
-            <div className="form-group">
-                <label>Members</label>
-                <input className="form-control"
-                    onChange={e=> setGroupCreator(e.target.value)} />
 
+            <div className="form-group">
+                <label htmlFor="membersList">Members</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="membersList"
+                    placeholder="Members"
+                    value = {form.id}
+                    onChange={(e) => setField("membersList", e.target.value)}
+                />
             </div>
 
             <div className="form-group">
                 <label htmlFor="date">Date</label>
-                <input type="Date" className="form-control" id="date" placeholder="date"/>
+                <input
+                    type="Date"
+                    className="form-control"
+                    id="date"
+                    placeholder="date"
+                    onChange={(e) => setField("date", e.target.value)}
+                />
             </div>
 
             <div className="form-group">
                 <label htmlFor="admin">Admin</label>
-                <input type="text" className="form-control" id="Admin" placeholder="admin"/>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="Admin"
+                    placeholder="admin"
+                    onChange={(e) => setField("Admin", e.target.value)}
+                />
             </div>
 
             <div className="form-group">
                 <label htmlFor="groupName">Group Name</label>
-                <input type="text" className="form-control" id="groupName" placeholder="Group name"/>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="groupName"
+                    placeholder="Group name"
+                    onChange={(e) => setField("groupName", e.target.value)}
+                />
             </div>
 
             <div className="form-group">
                 <label htmlFor="description">Description</label>
-                <textarea className="form-control" rows="3"></textarea>
+                <textarea
+                    className="form-control"
+                    id = "Description"
+                    placeholder= "Ex CS5500 study group"
+                    onChange={(e) => setField("Description", e.target.value)}
+                    rows="3">
+                </textarea>
             </div>
 
             <div className = 'mb-4 mt-2 text-center'>
-                <button type="button" className=" btn btn-primary btn-lg active me-2 ">Create Group </button>
-                <button type="button" className=" btn btn-primary btn-lg active me-2 " onClick={handleAddMembers}>Add Member</button>
+                <button
+                    type="button"
+                    className=" btn btn-primary btn-lg active me-2 "
+                    onClick={handleCreateGroup}
+                >Create Group
+                </button>
+
+                <button
+                    type="button"
+                    className=" btn btn-primary btn-lg active me-2 "
+                    onClick={handleAddMembers}
+                >Add Member
+                </button>
+
                 <button type="button" className=" btn btn-primary btn-lg active">Add Admin</button>
             </div>
 
 
         </Form>
-        // <div>
-        //     <h1>Create a group</h1>
-        //     <h3>Members</h3>
-        //     <input className="mb-2 form-control"/>
-        //     <h3>Date</h3>
-        //     <input className="mb-2 form-control"/>
-        //     <h3>Admins</h3>
-        //     <input className="mb-2 form-control"/>
-        //     <h3>Group Name</h3>
-        //     <input className="mb-2 form-control"/>
-        //     <h3>Group Description</h3>
-        //     <input className="mb-2 form-control"/>
-        //
-        // </div>
+
     );
 };
 export default CreateGroup;
