7bd3f4d4d62778c849720c7299167568117e686b
Gino <romangino@gmail.com>
2022-12-03 13:10:05 -0500

    Styling added for chat page

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -13,11 +13,25 @@ const Chat = () => {
             .then((group) => setGroup(group))
     useEffect(findGroupByGroupId, [])
     return(
-        <div className={'border border-light rounded-3 bg-success'}>
-            <div className={'row ps-1'}>
-                <h4 className={'pt-1'}>
-                    {group.groupName}
-                </h4>
+        <div className={'rounded-3 bg-light p-2'}>
+            <div className={'row ps-2'}>
+                <div className={'col p-2'}>
+                    <h4 className={'pt-1'}>
+                        {group.groupName}
+                    </h4>
+                </div>
+                <div className={'col p-2'}>
+                    <div className={'row pe-2'}>
+                        <div className='col-8 p-0'>
+                            <i className='fa-solid fa-gear fs-3 pt-1 pe-2 float-end text-secondary'></i>
+                        </div>
+                        <div className='col ps-0'>
+                            <button className='btn btn-danger float-end'>
+                                Leave
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className={'row'}>
                 <ChatList/>
