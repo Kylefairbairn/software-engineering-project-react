526b3d4708b6b533839f65dbde4b42ce088ac203
mitchneides <mitch.neides@gmail.com>
2022-12-07 12:42:42 -0500

    writes find all messages sent by user service

--- src/services/messages-service.js
+++ src/services/messages-service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const MESSAGES_API = `${BASE_URL}/api/messages`;
+const USERS_API = `${BASE_URL}/api/users`;
 
 
 /**
@@ -11,9 +12,14 @@ export const findAllMessages = () =>
     axios.get(`${MESSAGES_API}`)
         .then(response => response.data)
 
-
-
-// findAllMessagesSentByUser
+/**
+ * Find all messages that the specified user has sent
+ * @param uid User's primary key
+ * @returns {Promise<AxiosResponse<any>>} Promises to return the data from response
+ */
+export const findAllMessagesSentByUser = (uid) =>
+    axios.get(`${USERS_API}/${uid}/sent`)
+        .then(response => response.data)
 
 // findAllMessagesInGroup
 
