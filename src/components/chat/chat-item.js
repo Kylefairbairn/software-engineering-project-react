f85ae8a547aa9653134ff80ac9cc6614ca233da7
mitchneides <mitch.neides@gmail.com>
2022-12-09 17:21:30 -0500

    fixes edit/delete button appearance

--- src/components/chat/chat-item.js
+++ src/components/chat/chat-item.js
@@ -32,39 +32,31 @@ const ChatItem = ({message}) => {
 
     return (
 
-
         <li className='list-group-item'>
-            <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">@{sender.username}</h5>
-                <small>{date.toLocaleString()}</small>
-
-                <div id="adminButtons" className="btn-group btn-group-lg invisible" role="group" aria-label="Large button group">
-                    <a href="" className="pe-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             className="bi bi-pen" viewBox="0 0 16 16"><path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
-                        </svg>
-                    </a>
-                    <button type="button" className="btn-close" aria-label="Close"></button>
+            <div className="row">
+                <div className="col-3">
+                    <h5 className="mb-1">@{sender.username}</h5>
                 </div>
-
-                {
-                    currentUser._id === uid &&
-                    document.getElementById("adminButtons").setAttribute('class', 'btn-group btn-group-lg')
-                }
-
+                <div className="col-6">
+                    <small>{date.toLocaleString()}</small>
+                </div>
+                <div className="col-3">
+                    {
+                        currentUser._id === uid &&
+                        <div id="adminButtons" className="float-end" role="group" aria-label="Large button group">
+                            <a href="" className="pe-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                     className="bi bi-pen" viewBox="0 0 16 16"><path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+                                </svg>
+                            </a>
+                            <button type="button" className="btn-close" aria-label="Close"></button>
+                        </div>
+                    }
+                </div>
             </div>
+
             <br/>
             <p className="mb-1">{message.content}</p>
-
-
-
-            {/*<p>{currentUser}</p>*/}
-
-
-
-
-            {/*2. verify logged in user and check if it is their message*/}
-            {/*3. check if they are admin and show/hide buttons accordingly*/}
 
         </li>
 
