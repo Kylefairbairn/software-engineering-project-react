31f4757282a9836d43289f255f7640397907e455
kylefairbairn <kyle.fairbairn@gmail.com>
2022-12-11 15:54:52 -0500

    testing seems to be working

--- src/components/create/index.js
+++ src/components/create/index.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import {Form, FormControl} from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import {Alert, Form, FormControl} from "react-bootstrap";
 import AddMemberModal from "../create/AddMemberModal"
 import * as usersService from "../../services/users-service";
 import * as groupSerivce from "../../services/groups-service"
@@ -19,42 +19,47 @@ const CreateGroup = () => {
     })
     const [groupMembers, setGroupMembers] = useState([])
     const [admins, setAdmins] = useState([])
-    const [userNameError, setUserNameErrors] = useState(false)
+
+    const [emptyUsernameError, setEmptyUsernameErrors] = useState(false)
     const [createGroupError, setCreateGroupError] = useState(false)
     const [addMemberError, setAddMemberError] = useState(false)
     const [addAdminError, setAddAdminError] = useState(false)
-    const [dateError, setDateErrors] = useState(false)
-    const [setAdminError, setSetAdminErrors] = useState(false)
-    const [groupNameError, setGroupNameErrors] = useState(false)
+    const [emptyDateError, setEmptyDateErrors] = useState(false)
+    const [emptyAdminError, setEmptyAdminErrors] = useState(false)
+    const [emptyGroupNameError, setEmptyGroupNameErrors] = useState(false)
+    const [failedToAddMember, setFailedToAddMember] = useState(false)
+    const [failedToAddAdmin, setFailedToAddAdmin] = useState(false)
 
 
+    const [sucessfulAddMember, setSucessfulAddMember] = useState(false)
+    const [sucessfulAddAdmin, setSucessfullAddAdmin] = useState(false)
 
 
     const handleAddMembers = async () => {
 
-        if(form.username !== "") {
+        if (form.username !== "") {
             let user = await usersService.findUserByUsername(form.username)
 
             if (user !== null) {
                 setGroupMembers([...groupMembers, user._id])
-            }
-            else{
+                setSucessfulAddMember(true)
+            } else {
                 setAddMemberError(true)
             }
         }
-
+        //setAddMemberError(true)
     }
 
 
     const handleAddAdmin = async () => {
 
-        if(form.admin !== "") {
+        if (form.admin !== "") {
             let validAdmin = await usersService.findUserByUsername(form.admin)
 
             if (validAdmin !== null) {
-                setAdmins([...admins,validAdmin._id])
-            }
-            else{
+                setAdmins([...admins, validAdmin._id])
+                setSucessfullAddAdmin(true)
+            } else {
                 setAddAdminError(true)
             }
         }
@@ -66,41 +71,22 @@ const CreateGroup = () => {
         let flag = false
 
         if(form.username.length === 0){
-            setUserNameErrors(true)
+            setEmptyUsernameErrors(true)
             flag = true
         }
 
-        if(form.username.length > 0 && userNameError === true){
-            setUserNameErrors(false)
+        if (form.date.length === 0) {
+            setEmptyDateErrors(true)
             flag = true
         }
 
-        if (form.date.length === 0){
-            setDateErrors(true)
-            flag = true
-        }
-        if(form.date.length > 0 && dateError === true){
-            setDateErrors(false)
+        if (form.admin.length === 0) {
+            setEmptyAdminErrors(true)
             flag = true
         }
 
-        if (form.admin.length === 0){
-            setSetAdminErrors(true)
-            flag = true
-        }
-
-        if(form.admin.length > 0 && setAdminError === true){
-            setSetAdminErrors(false)
-            flag = true
-        }
-
-        if (form.groupName.length === 0){
-            setGroupNameErrors(true)
-            flag = true
-        }
-
-        if(form.groupName.length > 0 && groupNameError === true){
-            setGroupNameErrors(false)
+        if (form.groupName.length === 0) {
+            setEmptyGroupNameErrors(true)
             flag = true
         }
 
@@ -108,28 +94,61 @@ const CreateGroup = () => {
     }
 
 
-    const validFormEntries = async () => {
-        let user = await usersService.findUserByUsername(form.username)
-        let admin = await usersService.findUserByUsername(form.admin)
 
-        let flag = false
-        if(user === null || admin === null){
+    const validFormCheckOneEntry = async () => {
+
+        let flag = true
+        let users = {}
+        let newAdmin = {}
+
+        let found = false
+        let found2 = false
+        let lengthOfMembers = groupMembers.length
+
+        if(form.username !== '' && form.admin !== '') {
+            users = await usersService.findUserByUsername(form.username)
+            newAdmin = await usersService.findUserByUsername(form.admin)
+
+
+            for(let i = 0; i < lengthOfMembers; i++){
+                if(groupMembers[i] === users._id){
+                    found = true
+                }
+                if(admins[i] === newAdmin._id){
+                    found2 = true
+                }
+            }
+
+
+            if(users !== null && found === false){
+                setFailedToAddMember(true)
+                flag = false
+
+            }
+            if(newAdmin != null && found2 ===false){
+                setFailedToAddAdmin(true)
+                flag = false
+            }
+        }
+        else{
             flag = true
         }
+
         return flag
+
     }
 
     const createNewGroup = async () => {
 
         let emptyFormCheck = formEmptyEntryHandler()
-        let validFormCheck =  await validFormEntries()
-
-
+        let validCheck = await validFormCheckOneEntry()
         const currentUser = await authService.profile()
 
-        console.log(validFormCheck)
+        console.log(validCheck)
 
-        if(currentUser !== null && emptyFormCheck === false &&  validFormCheck === false) {
+        console.log(groupMembers)
+
+        if (currentUser !== null && emptyFormCheck === false && validCheck === true) {
 
             const group = {
                 members: groupMembers,
@@ -143,15 +162,45 @@ const CreateGroup = () => {
             navigate("/messages")
 
             console.log(status)
-        }
-        else{
+        } else {
             setCreateGroupError(true)
-            //alert("invalid group")
         }
     }
 
 
-    return(
+    useEffect(() => {
+
+        const timeId = setTimeout(() => {
+            if(addAdminError === true || addMemberError === true ||
+                emptyUsernameError === true || emptyDateError === true ||
+                emptyAdminError === true || emptyGroupNameError === true ||
+                sucessfulAddMember === true || sucessfulAddAdmin === true ||
+                createGroupError === true || failedToAddMember === true ||
+                failedToAddAdmin === true)
+
+            setAddAdminError(false)
+            setAddMemberError(false)
+            setEmptyUsernameErrors(false)
+            setEmptyDateErrors(false)
+            setEmptyAdminErrors(false)
+            setEmptyGroupNameErrors(false)
+            setSucessfulAddMember(false)
+            setSucessfullAddAdmin(false)
+            setCreateGroupError(false)
+            setFailedToAddMember(false)
+            setFailedToAddAdmin(false)
+        }, 3000)
+
+        return () =>{
+            clearTimeout(timeId)
+        }
+    }, [emptyUsernameError,emptyDateError,
+        emptyAdminError,emptyGroupNameError,
+        addMemberError,sucessfulAddMember,
+        sucessfulAddAdmin,addAdminError,
+        createGroupError, failedToAddMember, failedToAddAdmin])
+
+    return (
         <Form>
 
             <div className="form-group">
@@ -161,16 +210,16 @@ const CreateGroup = () => {
                     className="form-control"
                     id="membersList"
                     placeholder="Members"
-                    value = {form.username}
+                    value={form.username}
                     onChange={(e) => setForm({...form, username: e.target.value})}
 
                 />
             </div>
 
             <div className='' style={{color: 'red'}}>
-                {userNameError?
-                <label htmlFor='error' className='mt-2 mb-2' color='red' >
-                    Member cant be empty </label>:''}
+                {emptyUsernameError ?
+                    <label htmlFor='error' className='mt-2 mb-2' color='red'>
+                        Member cant be empty </label> : ''}
             </div>
 
 
@@ -181,14 +230,14 @@ const CreateGroup = () => {
                     className="form-control"
                     id="date"
                     placeholder="date"
-                    onChange = {(e) => setForm({...form, date: e.target.value})}
+                    onChange={(e) => setForm({...form, date: e.target.value})}
                 />
             </div>
 
             <div className='' style={{color: 'red'}}>
-                {dateError?
-                    <label htmlFor='error' className='mt-2 mb-2' color='red' >
-                        Date cant be empty </label>:''}
+                {emptyDateError ?
+                    <label htmlFor='error' className='mt-2 mb-2' color='red'>
+                        Date cant be empty </label> : ''}
             </div>
 
             <div className="form-group">
@@ -198,14 +247,14 @@ const CreateGroup = () => {
                     className="form-control"
                     id="Admin"
                     placeholder="admin"
-                    onChange = {(e) => setForm({...form, admin: e.target.value})}
+                    onChange={(e) => setForm({...form, admin: e.target.value})}
                 />
             </div>
 
             <div className='' style={{color: 'red'}}>
-                {setAdminError?
-                    <label htmlFor='error' className='mt-2 mb-2' color='red' >
-                        Admins cant be empty </label>:''}
+                {emptyAdminError ?
+                    <label htmlFor='error' className='mt-2 mb-2' color='red'>
+                        Admins cant be empty </label> : ''}
             </div>
 
             <div className="form-group">
@@ -215,32 +264,32 @@ const CreateGroup = () => {
                     className="form-control"
                     id="groupName"
                     placeholder="Group name"
-                    onChange = {(e) => setForm({...form, groupName: e.target.value})}
+                    onChange={(e) => setForm({...form, groupName: e.target.value})}
                 />
             </div>
 
             <div className='' style={{color: 'red'}}>
-                {groupNameError?
-                    <label htmlFor='error' className='mt-2 mb-2' color='red' >
-                        Group name cant be empty </label>:''}
+                {emptyGroupNameError ?
+                    <label htmlFor='error' className='mt-2 mb-2' color='red'>
+                        Group name cant be empty </label> : ''}
             </div>
 
             <div className="form-group">
                 <label htmlFor="description">Description</label>
                 <textarea
                     className="form-control"
-                    id = "Description"
-                    placeholder= "Ex CS5500 study group"
-                    onChange = {(e) => setForm({...form, description: e.target.value})}
+                    id="Description"
+                    placeholder="Ex CS5500 study group"
+                    onChange={(e) => setForm({...form, description: e.target.value})}
                     rows="3">
                 </textarea>
             </div>
 
 
-            <div className = 'mb-4 mt-4 text-center'>
+            <div className='mb-4 mt-4 text-center'>
                 <button
                     type="button"
-                    className=" btn btn-primary btn-lg active me-2 "
+                    className=" btn btn-primary btn-lg active me-2"
                     onClick={createNewGroup}
                 >Create Group
                 </button>
@@ -255,33 +304,52 @@ const CreateGroup = () => {
                 <button type="button"
                         className=" btn btn-primary btn-lg active"
                         onClick={handleAddAdmin}
-                >Add Admin</button>
-            </div>
-
-            <div className='text-center' style={{color: 'red'}}>
-                {addMemberError?
-                    <label htmlFor='error' className='mt-2 mb-2' color='red' >
-                        Can not add new member (not a valid username) </label>:''}
-            </div>
-
-            {/*<div className='text-center' style={{color: 'green'}}>*/}
-            {/*    {addMemberError?*/}
-            {/*        <label htmlFor='error' className='mt-2 mb-2'>*/}
-            {/*            Can not add new member (not a valid username) </label>:''}*/}
-            {/*</div>*/}
-
-            <div className='text-center' style={{color: 'red'}}>
-                {createGroupError?
-                    <label htmlFor='error' className='mt-2 mb-2' color='red' >
-                        Error when creating group! Try Again </label>:''}
+                >Add Admin
+                </button>
             </div>
 
 
+            <div className='text-center' style={{color: 'red'}}>
+                {failedToAddAdmin ?
+                    <label htmlFor='error' className='mt-2 mb-2' color='red'>
+                        Please press Add Admin button then press Create Group button </label> : ""}
+            </div>
 
             <div className='text-center' style={{color: 'red'}}>
-                {addAdminError?
-                    <label htmlFor='error' className='mt-2 mb-2' color='red' >
-                        Can not add new admin (not a valid username) </label>:''}
+                {addMemberError ?
+                    <label htmlFor='error' className='mt-2 mb-2' color='red'>
+                        Can not add new member (not a valid username) </label> : ""}
+            </div>
+
+            <div className='text-center' style={{color: 'red'}}>
+                {failedToAddMember ?
+                    <label htmlFor='error' className='mt-2 mb-2' color='red'>
+                        Please press Add Member button then press Create Group button </label> : ""}
+            </div>
+
+            <div className='text-center' style={{color: 'green'}}>
+                {sucessfulAddMember?
+                    <label htmlFor='error' className='mt-2 mb-2'>
+                        Member added </label>:''}
+            </div>
+
+            <div className='text-center' style={{color: 'red'}}>
+                {createGroupError ?
+                    <label htmlFor='error' className='mt-2 mb-2' color='red'>
+                        Error when creating group! Try Again </label> : ''}
+            </div>
+
+
+            <div className='text-center' style={{color: 'red'}}>
+                {addAdminError ?
+                    <label htmlFor='error' className='mt-2 mb-2' color='red'>
+                        Can not add new admin (not a valid username) </label> : ''}
+            </div>
+
+            <div className='text-center' style={{color: 'green'}}>
+                {sucessfulAddAdmin?
+                    <label htmlFor='error' className='mt-2 mb-2'>
+                        Admin Added</label> : ''}
             </div>
 
         </Form>
